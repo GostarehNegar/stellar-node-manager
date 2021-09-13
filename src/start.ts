e07fa93312922db0a9f716cf3be250101faf3e75
BabakMahmoudi <babak@gnco.ir>
2021-09-13 15:18:48 +0430

    start

--- src/start.ts
+++ src/start.ts
@@ -1,10 +1,14 @@
 import Docker from "dockerode"
+import {Node} from '@gostarehnegar/js-stellar-node-connector'
 console.log("hello world");
 const docker = new Docker({});
 
 
 (async ()=>{
 
+    let node = new Node(false,{
+        
+    });
     const list = await docker.listContainers()
     console.log(list);
 
